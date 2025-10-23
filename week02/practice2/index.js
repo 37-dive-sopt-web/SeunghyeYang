2a08f8abb375f20e9d3469009820c04fc0762fc1
bhyerain <seunghye0307@naver.com>
2025-10-24 02:38:37 +0900

    [Feat] Upload

---
+++ week02/practice2/index.js
@@ -0,0 +1,26 @@
+const addBtn = document.querySelector(".add-btn");
+const input = document.querySelector(".todo-input");
+const todoList = document.querySelector(".todo-list");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+todos.forEach((todo) => {
+  const li = document.createElement("li");
+  li.textContent = todo;
+  todoList.appendChild(li);
+});
+
+addBtn.addEventListener("click", () => {
+  const value = input.value;
+
+  if (!value) return;
+
+  const li = document.createElement("li");
+  li.textContent = value;
+  todoList.appendChild(li);
+
+  todos.push(value);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  input.value = "";
+});
