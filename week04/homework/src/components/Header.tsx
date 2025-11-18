fc8609104105450b497f97b6f38fb741ae942dbe
bhyerain <seunghye0307@naver.com>
2025-11-19 03:58:16 +0900

    [Feat] 헤더 심화과제 구현

--- week04/homework/src/components/Header.tsx
+++ week04/homework/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import * as styles from "./Header.css";
+import menu from "../assets/menu.svg";
+import { useState } from "react";
 
 interface HeaderProps {
   name: string;
@@ -9,11 +11,21 @@ interface HeaderProps {
 
 const Header = ({ name, activeTab, onOpenWithdrawModal }: HeaderProps) => {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem("userId");
     navigate("/");
     alert("로그아웃되었습니다.");
+  };
+  const handleClickInfo = () => {
+    navigate("/mypage");
+    setIsMenuOpen(false);
+  };
+
+  const handleClickUsers = () => {
+    navigate("/mypage/members");
+    setIsMenuOpen(false);
   };
 
   return (
@@ -45,6 +57,33 @@ const Header = ({ name, activeTab, onOpenWithdrawModal }: HeaderProps) => {
           회원탈퇴
         </button>
       </nav>
+
+      <button
+        type="button"
+        className={styles.menuButton}
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      >
+        <img src={menu} alt="메뉴" className={styles.menuIcon} />
+      </button>
+
+      <div
+        className={`${styles.mobileMenu} ${
+          isMenuOpen ? styles.mobileMenuOpen : ""
+        }`}
+      >
+        <button className={styles.mobileMenuItem} onClick={handleClickInfo}>
+          내 정보
+        </button>
+        <button className={styles.mobileMenuItem} onClick={handleClickUsers}>
+          회원 조회
+        </button>
+        <button className={styles.mobileMenuItem} onClick={handleLogout}>
+          로그아웃
+        </button>
+        <button className={styles.mobileMenuItem} onClick={onOpenWithdrawModal}>
+          회원탈퇴
+        </button>
+      </div>
     </header>
   );
 };
