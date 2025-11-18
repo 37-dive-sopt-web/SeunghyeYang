b48d4ab676f79953181fe0cf4e698c9a7d8ca577
bhyerain <seunghye0307@naver.com>
2025-11-18 20:04:35 +0900

    [Feat] Style 요소 세팅

---
+++ week04/homework/src/components/ThemeProvider.tsx
@@ -0,0 +1,10 @@
+// src/components/ThemeProvider.tsx
+import type { ReactNode } from "react";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return <>{children}</>;
+}
---
+++ week04/homework/src/styles/global.css.ts
@@ -0,0 +1,14 @@
+import { globalStyle } from "@vanilla-extract/css";
+import "./reset.css.ts";
+
+globalStyle("*, *::before, *::after", {
+  boxSizing: "border-box",
+});
+
+globalStyle("body", {
+  margin: 0,
+});
+
+globalStyle("#root", {
+  minHeight: "100vh",
+});
