1bcd2e468016d4936cf1f032f8581c81f64faea7
bhyerain <seunghye0307@naver.com>
2025-11-19 03:17:04 +0900

    [Feat] 비밀번호 에러 메시지 출력 기능 추가

--- week04/homework/src/pages/SignUp/PasswordStep.tsx
+++ week04/homework/src/pages/SignUp/PasswordStep.tsx
@@ -1,5 +1,6 @@
 import PasswordInputBox from "../../components/PasswordInput";
 import Button from "../../components/Button";
+import * as styles from "./signup.css";
 
 interface PasswordStepProps {
   password: string;
@@ -18,6 +19,25 @@ const PasswordStep = ({
 }: PasswordStepProps) => {
   const isDisabled =
     !password.trim() || !checkPassword.trim() || password != checkPassword;
+
+  const isMismatch =
+    password.length > 0 &&
+    checkPassword.length > 0 &&
+    password !== checkPassword;
+
+  const isValid =
+    /[A-Z]/.test(password) &&
+    /[a-z]/.test(password) &&
+    /[0-9]/.test(password) &&
+    /[^A-Za-z0-9]/.test(password) &&
+    !/\s/.test(password);
+
+  const isFull = isValid && checkPassword.length == 0;
+
+  const isShort =
+    (password.length > 0 && password.length < 8 && checkPassword.length < 1) ||
+    (password.length > 64 && checkPassword.length < 1);
+
   return (
     <>
       <PasswordInputBox
@@ -34,7 +54,18 @@ const PasswordStep = ({
         placeholder="비밀번호 확인"
         type="password"
       />
-
+      {isMismatch && (
+        <p className={styles.errorMessage}>비밀번호가 일치하지 않아요</p>
+      )}
+      {isFull && (
+        <p className={styles.errorMessage}>비밀번호를 모두 입력해주세요</p>
+      )}
+      {!isValid && isShort && (
+        <p className={styles.errorMessage}>
+          비밀번호는 공백 없이 8~64자이며, 대/소문자/숫자/특수문자를 각각 1자
+          이상 포함해야 합니다.
+        </p>
+      )}
       <Button type="button" disabled={isDisabled} onClick={onNext}>
         다음
       </Button>
